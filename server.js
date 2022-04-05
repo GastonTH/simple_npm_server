18046f6449f6323e5bcac570ed44644e772c1f5a
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2022-04-05 09:18:57 +0200

    project furniture

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const port = 8000;
 
-app.use(express.static('public'));
+app.use(express.static('public2'));
 
 http.createServer(app).listen(port, () => {
     console.log(`Server running on localhost:${port}`);
